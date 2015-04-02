5863b1aa58bf54e02cd3f45af61db8071da74721
HananGHannam <hanan.ghannam93@gmail.com>
2015-04-02 17:19:30 +0200

    #22 adding the search of the rest of categories

--- HeyBride/app/controllers/vendors_controller.rb
+++ HeyBride/app/controllers/vendors_controller.rb
@@ -1,114 +1,107 @@
+##
+# This class represents the categories in the website (hairdressers,make-up,dress stores..etc)
 class VendorsController < ApplicationController
-        before_action :authenticate_user!
-
+  before_action :authenticate_user!
   def new
   end
+
   def destroy
   @vendor = Vendor.find(params[:id])
   @vendor.destroy
- 
-redirect_to :controller => 'welcome', :action => 'HomePage'   
-end
-def index
-  
-end
+  redirect_to :controller => 'welcome', :action => 'HomePage'   
+  end
+  def index
+  end 
+
   def edit
     @vendor= Vendor.find(params[:id])
   end
  
-def update
+  def update
   @vendor = Vendor.find(params[:id])
- 
-  if @vendor.update(vendor_params)
-    redirect_to @vendor
-  else
+    if @vendor.update(vendor_params)
+      redirect_to @vendor
+    else
     render 'edit'
+    end
   end
-end
 
   def showPackages
-   @vendor= Vendor.find(params[:id])
-   end
+    @vendor= Vendor.find(params[:id])
+  end
 
   def showBrands
-   @vendor= Vendor.find(params[:id])
-   end
-
+    @vendor= Vendor.find(params[:id])
+  end
 
   def showSamples
-   @vendor= Vendor.find(params[:id])
-   end
+    @vendor= Vendor.find(params[:id])
+  end
 
-   def show
+  def show
     @vendor = Vendor.find(params[:id])
   end
-def indexHairdressers
- if params[:search]
-    @vendors = Vendor.search(params[:search]).where(entry:'Hairdresser').order("created_at DESC")
-  else
-    @vendors = Vendor.where(entry:'Hairdresser').order('created_at DESC')
-   end
-end
-   def indexDresses
- if params[:search]
-    @vendors = Vendor.search(params[:search]).where(entry:'Dress').order("created_at DESC")
-  else
-    @vendors = Vendor.where(entry:'Dress').order('created_at DESC')
-   end
+
+  def indexHairdressers
+    @q = Vendor.where(entry:'Hairdresser').ransack(params[:q])
+    @vendors = @q.result(:distinct=>true)
+    if @vendors.size.zero?
+      flash[:notice] = "No Matches Found"
     end
-    def indexMakeup
-if params[:search]
-    @vendors = Vendor.search(params[:search]).where(entry:'Makeup').order("created_at DESC")
-  else
-    @vendors = Vendor.where(entry:'Makeup').order('created_at DESC')
-   end 
- end
-    def indexPhotographers
-if params[:search]
-    @vendors = Vendor.search(params[:search]).where(entry:'Photographer').order("created_at DESC")
-  else
-    @vendors = Vendor.where(entry:'Photographer').order('created_at DESC')
-   end
+  end
+  def indexDresses
+    @q = Vendor.where(entry:'Dress').ransack(params[:q])
+    @vendors = @q.result(:distinct=>true)
+    if @vendors.size.zero?
+      flash[:notice] = "No Matches Found"
     end
-    def indexSuites
-if params[:search]
-    @vendors = Vendor.search(params[:search]).where(entry:'Suit').order("created_at DESC")
-  else
-    @vendors = Vendor.where(entry:'Suit').order('created_at DESC')
-   end 
- end
+  end
+  def indexMakeup
+   @q = Vendor.where(entry:'Makeup').ransack(params[:q])
+    @vendors = @q.result(:distinct=>true)
+    if @vendors.size.zero?
+      flash[:notice] = "No Matches Found"
+    end
+  end
+  def indexPhotographers
+   @q = Vendor.where(entry:'Photographer').ransack(params[:q])
+    @vendors = @q.result(:distinct=>true)
+    if @vendors.size.zero?
+      flash[:notice] = "No Matches Found"
+    end
+  end
+  def indexSuites
+   @q = Vendor.where(entry:'Suit').ransack(params[:q])
+    @vendors = @q.result(:distinct=>true)
+    if @vendors.size.zero?
+      flash[:notice] = "No Matches Found"
+    end
+  end
+
   def indexSpa
-if params[:search]
-    @vendors = Vendor.search(params[:search]).where(entry:'Spa').order("created_at DESC")
-  else
-    @vendors = Vendor.where(entry:'Spa').order('created_at DESC')
-   end 
- end
-     def indexDjs
+    @q = Vendor.where(entry:'Spa').ransack(params[:q])
+    @vendors = @q.result(:distinct=>true)
+    if @vendors.size.zero?
+      flash[:notice] = "No Matches Found"
+    end
+  end
 
-        
-                @q = Vendor.where(entry:'Dj').ransack(params[:q])
-                               @vendors = @q.result(:distinct=>true)
-
-        
-               if @vendors.size.zero?
-                               flash[:notice] = "No Matches Found"
-                             
-                               
-                  end
- 
-    
+  def indexDjs
+    @q = Vendor.where(entry:'Dj').ransack(params[:q])
+    @vendors = @q.result(:distinct=>true)
+    if @vendors.size.zero?
+      flash[:notice] = "No Matches Found"
+    end
   end
 
   def create
-  @vendor = Vendor.new(vendor_params)
- 
-  @vendor.save
-  redirect_to @vendor
-end
- 
-private
-  def vendor_params
-    params.require(:vendor).permit(:email, :telephone, :mobile, :address, :name, :image, :entry, :location, :capacity, :stars)
+    @vendor = Vendor.new(vendor_params)
+    @vendor.save
+    redirect_to @vendor
   end
-end
+ 
+  private
+    def vendor_params
+      params.require(:vendor).permit(:email, :telephone, :mobile, :address, :name, :image, :entry, :location, :capacity, :stars)
+    end
+  end
