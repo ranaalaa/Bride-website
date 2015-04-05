301f09103e462a0ac4e5c8bfba0593f5a1a45123
HananGHannam <hanan.ghannam93@gmail.com>
2015-04-05 12:58:20 +0200

    #22 removing rspec warning

---
+++ HeyBride/spec/controllers/vendors_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+require 'spec_helper'
+require 'support/controller_helper'
+
+
+RSpec.describe VendorsController, type: :controller do
+  describe "GET indexDjs" do
+    it "assigns @vendors" do
+      sign_in
+      vendor = Vendor.create!(:name => "Hanan", :entry=> "Dj")
+      get :indexDjs
+      expect(assigns(:vendors)).to eq([vendor])
+    end
+
+    it "renders the index template" do
+      sign_in 
+      get :indexDjs
+      expect(response).to render_template("indexDjs")
+    end
+  end
+end
--- HeyBride/spec/spec_helper.rb
+++ HeyBride/spec/spec_helper.rb
@@ -39,6 +39,9 @@ RSpec.configure do |config|
     # `true` in RSpec 4.
     mocks.verify_partial_doubles = true
   end
+   config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
