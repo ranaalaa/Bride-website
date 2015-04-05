14f99d32b2fae8d894dbeefe82ba3e16228a7d18
HananGHannam <hanan.ghannam93@gmail.com>
2015-04-05 23:48:43 +0200

    trying testing

--- HeyBride/spec/features/vendors_spec.rb
+++ HeyBride/spec/features/vendors_spec.rb
@@ -6,9 +6,10 @@ require 'support/controller_helper'
 RSpec.feature "Vendors", type: :feature do
 
   it "searches Djs" do
-   vendor = Vendor.create!(:name => "Hanan", :entry=> "Dj")
+   vendor = Vendor.create!(:name => "Hanan", :entry=> "Dj",:location => "Alex")
     visit 'vendors/indexDjs'
-    fill_in 'name_cont', :with => 'Hanan'
+    fill_in 'name', :with => 'Hanan'
+    fill_in 'location', :with => 'Alex'
     click_button 'search'
     expect(page).to have_content 'Hanan'
   end
