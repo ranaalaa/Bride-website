9f533c9b6945bac5d9eb918c53de6ce60e921ed9
HananGHannam <hanan.ghannam93@gmail.com>
2015-04-05 23:55:57 +0200

    trying testing

--- HeyBride/spec/features/vendors_spec.rb
+++ HeyBride/spec/features/vendors_spec.rb
@@ -7,9 +7,14 @@ RSpec.feature "Vendors", type: :feature do
 
   it "searches Djs" do
    vendor = Vendor.create!(:name => "Hanan", :entry=> "Dj",:location => "Alex")
+   vendor1 = Vendor.create!(:name => "Donia", :entry=> "Dj",:location => "Cairo")
+   vendor.packages.create!(:name => "package1", :price=> "200.0", :description=> "package1 has")
+
     visit 'vendors/indexDjs'
     fill_in 'name', :with => 'Hanan'
     fill_in 'location', :with => 'Alex'
+    fill_in 'min', :with => '300'
+
     click_button 'search'
     expect(page).to have_content 'Hanan'
   end
