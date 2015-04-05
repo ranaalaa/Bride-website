4ff721dcc9c01b9677d141c3b1bc51f46bf6fe1d
jala_ashraf <jala_ashraf_saeed@hotmail.com>
2015-04-05 14:01:28 +0000

    Added the external documentation and the UML

--- HeyBride/app/controllers/vendors_controller.rb
+++ HeyBride/app/controllers/vendors_controller.rb
@@ -28,14 +28,6 @@ end
    def show
     @vendor = Vendor.find(params[:id])
    end
- # Author:
- # Jala Ashraf
- # Description:
- # This action displays the index view of the Hairdressers in grid view in which it displays a row in group of 3 contating the image and below it the name of the Hairdressers
- # Success:
- # The user views the list of Hairdressers in a grid view of 3 per row
- # Failure:
- # The user views the list of Hairdressers in any other view but the grid view in group of 3 per row
 def indexHairdressers
  if params[:search]
     @vendors = Vendor.search(params[:search]).where(entry:'Hairdresser').order("created_at DESC")
@@ -57,14 +49,6 @@ if params[:search]
     @vendors = Vendor.where(entry:'Makeup').order('created_at DESC')
 end 
     end
- # Author:
- # Jala Ashraf
- # Description:
- # This action displays the index view of the Photographers in grid view in which it displays a row in group of 3 contating the image and below it the name of the Photographers
- # Success:
- # The user views the list of Photographers in a grid view of 3 per row
- # Failure:
- # The user views the list of Photographers in any other view but the grid view in group of 3 per row
     def indexPhotographers
 if params[:search]
     @vendors = Vendor.search(params[:search]).where(entry:'Photographer').order("created_at DESC")
@@ -86,14 +70,6 @@ if params[:search]
     @vendors = Vendor.where(entry:'Spa').order('created_at DESC')
 end 
   end
- # Author:
- # Jala Ashraf
- # Description:
- # This action displays the index view of the Djs in grid view in which it displays a row in group of 3 contating the image and below it the name of the Dj
- # Success:
- # The user views the list of Djs in a grid view of 3 per row
- # Failure:
- # The user views the list of Djs in any other view but the grid view in group of 3 per row
      def indexDjs
 if params[:search]
     @vendors = Vendor.search(params[:search]).where(entry:'Dj').order("created_at DESC")
@@ -104,22 +80,20 @@ end
  # Author:
  # Jala Ashraf
  # Description:
- # This action displays the index view of the Halls in grid view in which it displays a row in group of 3 contating the image and below it the name of the Hall and its location
- # Success:
- # The user views the list of halls in a grid view of 3 per row
+ # This action displays all the list of Halls
+ # The user views all the list of Halls
  # Failure:
- # The user views the list of halls in any other view but the grid view in group of 3 per row
+ # The user doesn't views the list of Halls
     def indexHall
     @vendors = Vendor.where(entry:'Hall').order('created_at DESC')
     end
  # Author:
  # Jala Ashraf
  # Description:
- # This action displays the index view of in grid view in which it displays a row in group of 3 contating the image and below it the name of the Garden and its location
- # Success:
- # The user views the list of Gardens in a grid view of 3 per row
+ # This action displays all the list of Gardens
+ # The user views the all the list of Gardens
  # Failure:
- # The user views the list of Gardens in any other view but the grid view in group of 3 per row
+ # The user doesn't views the list of Gardens
     def indexGarden
     @vendors = Vendor.where(entry:'Garden').order('created_at DESC')
     end
