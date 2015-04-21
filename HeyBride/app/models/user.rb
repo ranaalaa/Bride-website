2a37cc09050089873691e11faf11a4da99c701a0
HananGHannam <hanan.ghannam93@gmail.com>
2015-04-21 23:05:00 +0200

    #46 making the username a required field

--- HeyBride/app/models/user.rb
+++ HeyBride/app/models/user.rb
@@ -7,6 +7,8 @@ class User < ActiveRecord::Base
 
          devise :omniauthable, :omniauth_providers => [:facebook]
          def self.from_omniauth(auth)
+  validates :username, presence: true
+  validates :username, uniqueness: true, if: -> { self.username.present? }
          	
   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
     user.email = auth.info.email
