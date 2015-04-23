54aaa833fc4845459ad83bd0b1999f53d60a0fd3
HananGHannam <hanan.ghannam93@gmail.com>
2015-04-23 21:54:45 +0200

    #55 modifying the test

--- HeyBride/spec/features/welcomes_spec.rb
+++ HeyBride/spec/features/welcomes_spec.rb
@@ -8,7 +8,7 @@ RSpec.feature "Welcomes", type: :feature do
     before { login_as(user, scope: :user )}
     it "returns vendors" do
       vendor = Vendor.create!(:name => "Hanan", :entry=> "Dj",:location => "Alex")
-      vendor1 = Vendor.create!(:name => "Hanan", :entry=> "Haidresser",:location => "Cairo")
+      vendor1 = Vendor.create!(:name => "Hanan", :entry=> "Hairdresser",:location => "Cairo")
       visit 'welcome/HomePage'
       fill_in 'Search by name', :with => 'Hanan'
       click_button 'submit'
@@ -16,7 +16,7 @@ RSpec.feature "Welcomes", type: :feature do
     end
     it "returns matches not found" do
       vendor = Vendor.create!(:name => "Hanan", :entry=> "Dj",:location => "Alex")
-      vendor1 = Vendor.create!(:name => "Hanan", :entry=> "Haidresser",:location => "Cairo")
+      vendor1 = Vendor.create!(:name => "Hanan", :entry=> "Hairdresser",:location => "Cairo")
       visit 'welcome/HomePage'
       fill_in 'Search by name', :with => 'Donia'
       click_button 'submit'
