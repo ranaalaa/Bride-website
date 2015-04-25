f6e86050016237c5ff5d370d6484af6356afa13c
HananGHannam <hanan.ghannam93@gmail.com>
2015-04-25 13:26:55 +0300

    #28 adding the user name beside the comments for facebook users

--- HeyBride/db/schema.rb
+++ HeyBride/db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150421211610) do
     t.integer  "vendor_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "commenter"
   end
 
   add_index "comments", ["vendor_id"], name: "index_comments_on_vendor_id"
@@ -68,12 +69,12 @@ ActiveRecord::Schema.define(version: 20150421211610) do
   add_index "samples", ["vendor_id"], name: "index_samples_on_vendor_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",   null: false
+    t.string   "encrypted_password",     default: "",   null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,    null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -84,7 +85,7 @@ ActiveRecord::Schema.define(version: 20150421211610) do
     t.string   "uid"
     t.string   "username"
     t.boolean  "admin"
-    t.boolean  "female"
+    t.boolean  "female",                 default: true
     t.string   "phone"
   end
 
